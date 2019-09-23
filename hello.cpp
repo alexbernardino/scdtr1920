dbeab0a7863e82aaf503b8d7f1f34bbc90aa0a0e
alexbernardino <alex@isr.ist.utl.pt>
2019-09-23 09:14:52 +0100

    Examples for Module 4

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Alexandre Bernardino on 2019-09-23.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string hello = "Hello World";
+    std::cout << hello << std::endl;
+}
---
+++ sizeof.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Alexandre Bernardino on 2019-09-22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "bool: " << sizeof(bool) << endl;
+    cout << "char: " << sizeof(char) << endl;
+    cout << "int: " << sizeof(int) << endl;
+    cout << "short int: " << sizeof(short int) << endl;
+    cout << "long int: " << sizeof(long int) << endl;
+    cout << "float: " << sizeof(float) << endl;
+    cout << "double: " << sizeof(double) << endl;
+    cout << "long double: " << sizeof(long double) << endl;
+}
